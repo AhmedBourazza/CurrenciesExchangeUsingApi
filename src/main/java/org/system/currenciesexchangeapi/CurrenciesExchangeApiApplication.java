c2ab95f5b5d9723eaf40870640e6a358609bf0e9
Ahmed Bourazza <ahmedbourazza1@gmail.com>
2025-09-04 17:45:14 +0200

    first commit

---
+++ src/main/java/org/system/currenciesexchangeapi/CurrenciesExchangeApiApplication.java
@@ -0,0 +1,13 @@
+package org.system.currenciesexchangeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrenciesExchangeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrenciesExchangeApiApplication.class, args);
+    }
+
+}
